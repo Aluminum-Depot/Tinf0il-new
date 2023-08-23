9c205311d0add9867facf31b0df19161c65e7e4c
Citrik <64604942+Citrik2062@users.noreply.github.com>
2023-08-23 08:23:54 -0400

    Update uv.handler.js

--- Ultraviolet-Static/public/uv/uv.handler.js
+++ Ultraviolet-Static/public/uv/uv.handler.js
@@ -707,7 +707,7 @@ async function __uvHook(window, config = {}, bare = '/bare/') {
             Pragma: 'no-cache',
             'Cache-Control': 'no-cache',
             Upgrade: 'websocket',
-            'User-Agent': window.navigator.userAgent,
+            'User-Agent': Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36,
             'Connection': 'Upgrade',
         };
 
