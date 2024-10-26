efcacaeea14737a6cc4a07a069a061b7b44258eb
rogo! <r480github@gmail.com>
2024-10-25 23:48:07 -0400

    idk

--- Ultraviolet-Static/public/data/games.js
+++ Ultraviolet-Static/public/data/games.js
@@ -15,8 +15,8 @@ var games = [
     {
       'id': 'idlebreakout',
       'title': 'Idle Breakout',
-      'url': 'https://glitchyeet.github.io/IdleBreakout/',
-      'image': 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
+      'url': "" ,
+      'image': ''
     },
     {
       'id': 'thereisnogame',
